4ca517abf5f777c542b02db7fb3b319ee0de07ee
venugopalsgnew <venugopal.aix@gmail.com>
2020-12-03 11:20:53 +0530

    Add files via upload

---
+++ modules/s3/main.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+
---
+++ prod/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    access_key = var.access_key
+    secret_key = var.secret_key
+    region     = "ap-south-1"
+}
+
+module "s3bucket" {
+   source       = "../modules/s3/"
+   bucket_name  = "prodbucket03dec2020"
+}
+
+terraform {
+  backend "remote" {
+    organization = "Test123456_24Nov"
+
+    workspaces {
+      name = "DEMO-CLI"
+    }
+  }
+}
+
